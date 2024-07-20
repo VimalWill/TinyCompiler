ddec333030b43c222a3e13f4ca07bd908c7672f9
VimalWill <vimalwilliam99@gmail.com>
2024-07-20 21:04:15 +0530

    working on fusion dialect

---
+++ Compiler/Src/TinyCompilerOpt.cpp
@@ -0,0 +1,9 @@
+/* ---- optimizer for tiny compiler ---- */
+
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+
+int main(int* argc, char** argv) {
+
+    //TODO: custom pass for operator fusion
+}
